96fe7af66594a66fdb53ee0c3b548adc5ff5ef4c
xuanzebin <xuanzebin@126.com>
2018-08-13 00:10:07 +0800

    add eventHub

--- src/js/songsForm.js
+++ src/js/songsForm.js
@@ -1,20 +1,23 @@
 {
     let view={
         el:'main',
+        init(){
+            this.$el=$(this.el)
+        },
         template:`
         <form>
             <h1>编辑新建歌曲</h1>
             <div class="row">
                 <label>歌曲：</label>
-                <input name="name" type="text">
+                <input name="name" type="text" value="__name__">
             </div>
             <div class="row">
                 <label>歌手：</label>
-                <input name="singer" type="text">
+                <input name="singer" type="text"  value="__singer__">
             </div>
             <div class="row">
                 <label>外链地址：</label>
-                <input name="url" type="text">
+                <input name="url" type="text" value="__url__">
             </div>
             <div class="row">
                 <input type="submit" value="save"></button>
@@ -22,15 +25,30 @@
         </form>
         `,
         render(data){
-            $(this.el).html(this.template)
+            let playholders=['name','singer','url']
+            let html=this.template
+            playholders.map((string)=>{
+                html=html.replace(`__${string}__`,data[string])
+            })
+            $(this.el).html(html)
         }
     }
-    let model={}
+    let model={
+        data:{name:'',singer:'',url:''}
+    }
     let controller={
         init(view,model){
             this.view=view
+            this.view.init()
             this.model=model
+            this.bindEvents()
             this.view.render(this.model.data)
+        },
+        bindEvents(){
+            this.view.$el.on('submit','form',(e)=>{
+                e.preventDefault()
+                alert(1)
+            })
         }
     }
     controller.init(view,model)
